586e8d657f29d5c039f95b72cb37e611489fd179
Гаджиев Эльдар Ражидинович <ERGadzhiev@sberbank.ru>
2022-11-19 04:51:32 +0300

    connected Apollo client

--- App.tsx
+++ App.tsx
@@ -1,22 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
+
+const API_KEY =
+  'reston::stepzen.net+1000::51b3170c61439c96b74fbe94fbde4c314751f07dea53993b06454e479fd25fc5'
+
+const client = new ApolloClient({
+  uri: 'https://reston.stepzen.net/api/ill-stingray/__graphql',
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+})
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
