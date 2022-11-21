908c141de17a0bb5e4f15491b82f299ae3308d1e
Гаджиев Эльдар Ражидинович <ERGadzhiev@sberbank.ru>
2022-11-21 14:14:16 +0300

    added search, tabs

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,7 +1,18 @@
-import { gql, useQuery } from '@apollo/client'
-import { ActivityIndicator, FlatList, StyleSheet } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { gql, useLazyQuery } from '@apollo/client'
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native'
 import BookItem from '../components/BookItem/BookItem'
 import { Text, View } from '../components/Themed'
+
+import { debounce } from 'lodash'
+import { IBook } from '../types/IBook'
 
 const query = gql`
   query SearchBooks($q: String) {
@@ -36,10 +47,59 @@ const query = gql`
 `
 
 export default function TabOneScreen() {
-  const { data, loading, error } = useQuery(query, { variables: { q: 'React Native' } })
-  console.log({ data })
+  const [searchValue, setSearchValue] = useState<string>('')
+  const [sendQuery, { data, loading, error }] = useLazyQuery(query)
+  const [provider, setProvider] = useState<'googleBooksSearch' | 'openLibrarySearch'>(
+    'googleBooksSearch',
+  )
+
+  const loadData = useCallback(debounce(sendQuery, 300), [])
+
+  const onSearchHandler = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    setSearchValue(e.nativeEvent.text)
+    loadData({ variables: { q: searchValue } })
+  }
+
+  const getBookDataFrom = (item: any): IBook => {
+    if (provider === 'googleBooksSearch') {
+      return {
+        image: item.volumeInfo?.imageLinks?.thumbnail,
+        title: item.volumeInfo.title,
+        authors: item.volumeInfo.authors,
+        isbn: item.volumeInfo?.industryIdentifiers?.[0]?.identifier,
+      }
+    }
+    return {
+      image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+      title: item.title,
+      authors: item.author_name,
+      isbn: item.isbn,
+    }
+  }
+
+  useEffect(() => {
+    loadData({ variables: { q: 'React Native' } })
+  }, [])
+
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <TextInput style={styles.input} placeholder='Search...' onChange={onSearchHandler} />
+      </View>
+
+      <View style={styles.tabs}>
+        <Text
+          style={provider === 'googleBooksSearch' ? { fontWeight: 'bold', color: 'royalblue' } : {}}
+          onPress={() => setProvider('googleBooksSearch')}>
+          Google Books
+        </Text>
+        <Text
+          style={provider === 'openLibrarySearch' ? { fontWeight: 'bold', color: 'royalblue' } : {}}
+          onPress={() => setProvider('openLibrarySearch')}>
+          Open Library
+        </Text>
+      </View>
+
       {loading ? (
         <ActivityIndicator />
       ) : error ? (
@@ -49,17 +109,18 @@ export default function TabOneScreen() {
       ) : (
         <FlatList
           showsVerticalScrollIndicator={false}
-          data={data.googleBooksSearch.items || []}
-          renderItem={({ item }) => (
-            <BookItem
-              book={{
-                image: item.volumeInfo.imageLinks.thumbnail,
-                title: item.volumeInfo.title,
-                authors: item.volumeInfo.authors,
-                isbn: item.volumeInfo.industryIdentifiers[0].identifier,
-              }}
-            />
+          data={
+            (provider === 'googleBooksSearch'
+              ? data?.googleBooksSearch?.items
+              : data?.openLibrarySearch.docs) || []
+          }
+          keyExtractor={(item) => item.id}
+          ListEmptyComponent={() => (
+            <View style={styles.noData}>
+              <Text>No data</Text>
+            </View>
           )}
+          renderItem={({ item }) => <BookItem book={getBookDataFrom(item)} />}
         />
       )}
     </View>
@@ -79,5 +140,24 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  header: {
+    marginVertical: 15,
+  },
+  input: {
+    borderColor: '#cdcdcd',
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+  },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 50,
+  },
+  noData: {
+    alignItems: 'center',
   },
 })
